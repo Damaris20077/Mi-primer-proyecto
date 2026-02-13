63014caa1a4048e1fbc8f07df1ff86f254ebfafa
Daniela AG <davilez@estud.tesa.edu.ec>
2026-02-13 13:37:27 -0500

    primer commit

---
+++ android/app/src/main/java/ionic/daniela1/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.daniela1;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.daniela1',
+  appName: 'daniela1',
+  webDir: 'dist'
+};
+
+export default config;
